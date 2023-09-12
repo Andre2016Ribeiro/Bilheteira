6e61736b1bf9828131ac67d7260bd7c7ebc44e26
Andre.Ribeiro <vitor.andre.ribeiro@gmail.com>
2023-09-12 15:51:46 +0100

    start chang

--- WebBilheteira/Startup.cs
+++ WebBilheteira/Startup.cs
@@ -22,7 +22,7 @@ namespace WebBilheteira
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the cont.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
